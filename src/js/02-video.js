1f72f4ccbe335204b006b8541eb4092ae630b35b
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-11 16:32:51 +0300

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-console.log(throttle);
 
-console.log(Player);
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
