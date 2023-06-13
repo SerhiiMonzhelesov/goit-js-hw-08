078cb2d2e15619b2929f5721467c819e979a7741
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-13 18:11:22 +0300

    js hw8 task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const INPUTS_VALUE = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+
+form.addEventListener('input', throttle(handlerSaveInputsValue, 500));
+form.addEventListener('submit', handlerSubmitForm)
+
+function handlerSaveInputsValue(event) {
+    INPUTS_VALUE[event.target.name] = event.target.value   
+    localStorage.setItem("feedback-form-state", JSON.stringify(INPUTS_VALUE)) 
+}
+
+function restoreInput() {
+    for (let elem of form) {
+        if (INPUTS_VALUE && Object.keys(INPUTS_VALUE).includes(elem.name)) {
+            elem.value = INPUTS_VALUE[elem.name]
+        }
+    }  
+}
+
+restoreInput()
+
+function handlerSubmitForm(event) {
+    event.preventDefault();
+    console.log(INPUTS_VALUE);
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state")
+}
